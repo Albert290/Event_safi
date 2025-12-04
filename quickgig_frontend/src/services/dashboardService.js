cc668e7f31e08332dc00fa3e8c86dbb76778f014
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-12-04 17:22:55 +0300

    chore: Remove deleted folder quickgig_frontend.

--- quickgig_frontend/src/services/dashboardService.js
+++
@@ -1,9 +0,0 @@
-import { PRIVATE_URL } from "./api";
-
-export const getClientTasks = () => {
-    return PRIVATE_URL.get("tasks/bookings/client/");
-};
-
-export const getTaskerTasks = () => {
-    return PRIVATE_URL.get("tasks/bookings/tasker/");
-};
